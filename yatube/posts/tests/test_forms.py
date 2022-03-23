28c9a919a27f4cbe8ce1c7fc6fdcf1e92f4eb59b
Иван Варюхин <ivan@varyukhin.ru>
2022-03-23 16:37:25 +0300

    pep8 fix

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -14,6 +14,7 @@ User = get_user_model()
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class FormsPostCreateTests(TestCase):
     @classmethod
